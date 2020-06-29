b619c8e292b2a8aac5a44826114d7972bc7bf3f7
Bartosz Pieniak <bartoszpieniak@outlook.com>
2020-06-29 20:05:30 +0200

    first commit

---
+++ Choroba.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Szpital
+{
+    public class Choroba
+    {
+        public string nazwa;
+        public string typ;
+
+        public Choroba(string nazwa, string typ)
+        {
+            this.nazwa = nazwa;
+            this.typ = typ;
+        }
+    }
+}
